7eea98557a06baee05b257f58a6d3a0424e57e0c
Luiz Pires <lpiresjunior@gmail.com>
2023-06-16 18:41:10 -0300

    feat: handle animation events

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,5 +1,5 @@
 import { Dimensions, Text } from 'react-native'
-import Animated, { Keyframe } from 'react-native-reanimated'
+import Animated, { Keyframe, runOnJS } from 'react-native-reanimated'
 
 import { Option } from '../Option'
 import { styles } from './styles'
@@ -13,6 +13,7 @@ interface Props {
   question: QuestionProps
   alternativeSelected?: number | null
   setAlternativeSelected?: (value: number) => void
+  onUnmount: () => void
 }
 
 const SCREEN_WIDTH = Dimensions.get('window').width
@@ -20,30 +21,43 @@ const SCREEN_WIDTH = Dimensions.get('window').width
 export function Question ({
   question,
   alternativeSelected,
-  setAlternativeSelected
+  setAlternativeSelected,
+  onUnmount
 }: Props) {
   const enteringKeyFrame = new Keyframe({
     0: {
       opacity: 0,
-      transform: [{ translateX: SCREEN_WIDTH }, { rotate: '90deg' }]
+      transform: [
+        { translateX: SCREEN_WIDTH },
+        { rotate: '90deg' }
+      ]
     },
     70: {
       opacity: 0.3
     },
     100: {
       opacity: 1,
-      transform: [{ translateX: 0 }, { rotate: '0deg' }]
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg' }
+      ]
     }
   })
 
   const exitingKeyFrame = new Keyframe({
     from: {
       opacity: 1,
-      transform: [{ translateX: 0 }, { rotate: '0deg' }]
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg' }
+      ]
     },
     to: {
       opacity: 0,
-      transform: [{ translateX: SCREEN_WIDTH * -1 }, { rotate: '-90deg' }]
+      transform: [
+        { translateX: SCREEN_WIDTH * (-1) },
+        { rotate: '-90deg' }
+      ]
     }
   })
 
@@ -51,20 +65,27 @@ export function Question ({
     <Animated.View
       style={styles.container}
       entering={enteringKeyFrame.duration(400)}
-      exiting={exitingKeyFrame.duration(400)}
+      exiting={exitingKeyFrame.duration(400).withCallback((finished) => {
+        'worklet'
+        if (finished) {
+          runOnJS(onUnmount)
+        }
+      })}
     >
-      <Text style={styles.title}>{question.title}</Text>
+      <Text style={styles.title}>
+        {question.title}
+      </Text>
 
-      {question.alternatives.map((alternative, index) => (
-        <Option
-          key={index}
-          title={alternative}
-          checked={alternativeSelected === index}
-          onPress={() => {
-            setAlternativeSelected != null && setAlternativeSelected(index)
-          }}
-        />
-      ))}
+      {
+        question.alternatives.map((alternative, index) => (
+          <Option
+            key={index}
+            title={alternative}
+            checked={alternativeSelected === index}
+            onPress={() => { setAlternativeSelected && setAlternativeSelected(index) }}
+          />
+        ))
+      }
     </Animated.View>
   )
 }
